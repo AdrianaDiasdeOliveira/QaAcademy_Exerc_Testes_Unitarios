5a30f4f6f1a359599fe3a228852d8328032084ab
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-03-09 10:57:07 -0300

    Teste Unitario 06 Easy

--- src/main/java/TestesUnitarios_Easy/TesteUnitarioEx06.java
+++ src/main/java/TestesUnitarios_Easy/TesteUnitarioEx06.java
@@ -7,13 +7,11 @@ import org.junit.Test;
 
 public class TesteUnitarioEx06 {
 
-
+@Test
     public void TestverificarAprovacaoAluno(){
-        Exercicio06_easy exercicio06Easy = new Exercicio06_easy();
-
-
-
-
-
-    }
+    Exercicio06_easy exercicio06Easy = new Exercicio06_easy();
+    Assert.assertEquals("Você está aprovado!!!", exercicio06Easy.verificarAprovacaoAluno(5,6));
+    Assert.assertEquals("Você está de exame!!!", exercicio06Easy.verificarAprovacaoAluno(5,5));
+    Assert.assertEquals("Você está reprovado!!!", exercicio06Easy.verificarAprovacaoAluno(4,4));
+  }
 }
