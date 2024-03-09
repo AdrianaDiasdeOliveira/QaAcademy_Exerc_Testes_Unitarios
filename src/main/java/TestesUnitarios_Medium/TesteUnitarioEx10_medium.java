840a824622389f4b12277f54eec7b1c63991adc4
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-03-09 15:43:41 -0300

    Testes Funcionais Ex 10 medio

--- src/main/java/TestesUnitarios_Medium/TesteUnitarioEx10_medium.java
+++ src/main/java/TestesUnitarios_Medium/TesteUnitarioEx10_medium.java
@@ -1,4 +1,29 @@
 package TestesUnitarios_Medium;
 
+import easy.Exercicio06_easy;
+import medium.Exercicio10_medium;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TesteUnitarioEx10_medium {
+    int[] vetorEsperado = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+    Exercicio10_medium exercicio10Medium = new Exercicio10_medium();
+
+    @Before
+    public void before() {
+        exercicio10Medium = new Exercicio10_medium();
+    }
+
+    @Test
+    public void testeFibonacci() {
+        Assert.assertArrayEquals(vetorEsperado, exercicio10Medium.calcularFibonacci(10));
+    }
+
+    @Test
+    public void testeExibeFibonacci() {
+
+        exercicio10Medium.exibirVetor(vetorEsperado);
+    }
+
 }
