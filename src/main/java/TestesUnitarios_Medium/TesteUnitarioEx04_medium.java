e9197997c3660a1364dda88403a846c15cfaf766
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-03-09 14:39:12 -0300

    teste unitario 04 -medio

--- src/main/java/TestesUnitarios_Medium/TesteUnitarioEx04_medium.java
+++ src/main/java/TestesUnitarios_Medium/TesteUnitarioEx04_medium.java
@@ -1,4 +1,26 @@
 package TestesUnitarios_Medium;
 
+import easy.Exercicio06_easy;
+import medium.Exercicio04_medium;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TesteUnitarioEx04_medium {
+    int[] vetorMultiplicaAteMilEsperado;
+    Exercicio04_medium exercicio04Medium;
+
+    @Before
+    public void before() {
+        vetorMultiplicaAteMilEsperado = new int[]{1, 2, 6, 24};
+        exercicio04Medium = new Exercicio04_medium();
+    }
+
+    @Test
+    public void testeSomeAteMil() {
+        int[] vetorAtual = exercicio04Medium.multiplicaAteMilIteracoes();
+        for (int i = 0; i < vetorMultiplicaAteMilEsperado.length; i++) {
+            Assert.assertEquals(vetorMultiplicaAteMilEsperado[i], vetorAtual[i]);
+        }
+    }
 }
