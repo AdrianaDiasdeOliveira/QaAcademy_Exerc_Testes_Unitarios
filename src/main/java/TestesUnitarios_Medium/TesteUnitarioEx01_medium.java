79812864456a282da1f831930c9dbf9c413e7f70
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-03-09 14:30:48 -0300

    testes unitarios 03 e as cÃlass main do 01 e o2

--- src/main/java/TestesUnitarios_Medium/TesteUnitarioEx01_medium.java
+++ src/main/java/TestesUnitarios_Medium/TesteUnitarioEx01_medium.java
@@ -1,4 +1,31 @@
 package TestesUnitarios_Medium;
 
+import easy.Exercicio06_easy;
+import medium.Exercicio01_medium;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TesteUnitarioEx01_medium {
-}
+         Exercicio01_medium exercicio01Medium;
+
+        @Before
+        public void before() {
+            exercicio01Medium = new Exercicio01_medium();
+        }
+
+        @Test
+        public void testeVerificaPar() {
+            String msgEsperada = "O Numero: 2 - é Par";
+            String msgValorAtual = exercicio01Medium.verificaParOuImpar(2);
+            Assert.assertEquals(msgEsperada, msgValorAtual);
+        }
+
+        @Test
+        public void testeVerificaImpar() {
+            String msgEsperada = "O Numero: 1 - é ímpar";
+            String msgValorAtual = exercicio01Medium.verificaParOuImpar(1);
+            Assert.assertEquals(msgEsperada, msgValorAtual);
+        }
+    }
+
--- src/main/java/TestesUnitarios_Medium/TesteUnitarioEx02_medium.java
+++ src/main/java/TestesUnitarios_Medium/TesteUnitarioEx02_medium.java
@@ -1,4 +1,29 @@
 package TestesUnitarios_Medium;
 
+import easy.Exercicio06_easy;
+import medium.Exercicio02_medium;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TesteUnitarioEx02_medium {
+    Exercicio02_medium exercicio02Medium;
+
+    @Before
+    public void before() {
+        exercicio02Medium = new Exercicio02_medium();
+
+    }
+
+    @Test
+    public void testeCalcularInvstimentoJurosCompostos() {
+        Assert.assertEquals("31058.482083442083", exercicio02Medium.calcularInvstimentoJurosCompostos(10000.00).toString());
+    }
+
+    @Test
+    public void testeCalcularValorDosJuros() {
+        Double valorTotal = 31058.482083442083;
+        Double valorInvestido = 10000.00;
+        Assert.assertEquals("21058.482083442083", exercicio02Medium.calcularValorDosJuros(valorTotal, valorInvestido).toString());
+    }
 }
