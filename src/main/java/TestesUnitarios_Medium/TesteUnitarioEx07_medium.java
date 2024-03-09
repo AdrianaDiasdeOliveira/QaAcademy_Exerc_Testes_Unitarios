93411b07610d0ad3a1e649d5a9d5c3ae43d7060b
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-03-09 14:59:12 -0300

    Teste Unitario Ex_Medio 07 e 08

--- src/main/java/TestesUnitarios_Medium/TesteUnitarioEx07_medium.java
+++ src/main/java/TestesUnitarios_Medium/TesteUnitarioEx07_medium.java
@@ -1,16 +1,20 @@
 package TestesUnitarios_Medium;
 
+import easy.Exercicio02_easy;
+import easy.Exercicio06_easy;
+import easy.Exercicio07_easy;
+import medium.Exericio07_08_medium;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TesteUnitarioEx07_medium {
+    String[] vetorEsperado = new String[]{"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
 
     @Test
-
-    public void testVetorMeses(){
-
+    public void TesteUnitarioEx07_medium() {
+            Exericio07_08_medium  exericio07_08_medium =new Exericio07_08_medium();
+            Assert.assertEquals(vetorEsperado,exericio07_08_medium.exibirMesesDoAno());
 
     }
-
 
 }
